f0b98e1441c8960c6a74cea986654a7882a0f182
Viktor Nemov <vik.nemov.vas@yandex.ru>
2022-10-19 11:12:26 +0300

    bugfix(master): Исправил работу табов

--- src/blocks/modules/news/news.ts
+++ src/blocks/modules/news/news.ts
@@ -1,14 +1,15 @@
-const newsTarget = document.querySelectorAll('.news__item')!;
-const infoTarget = document.querySelectorAll('.info-list__item')!;
+const newsItem = document?.querySelectorAll('.news__item')!;
+const infoItem = document?.querySelectorAll('.info-list__item')!;
 
-    if(newsTarget){
-        newsTarget.forEach((item, index) => {
-            
-            item.addEventListener('click', () => {
-                infoTarget.forEach(item => {
-                    item.classList.remove('info-list__item--active')
-                })
-                infoTarget[index].classList.add('info-list__item--active')
-            })
-        })
-    }
+newsItem?.forEach((item1, index) => {
+  item1.addEventListener('click', () => {
+    newsItem?.forEach((item) => {
+        item.classList.remove('news__item--active');
+      });
+    infoItem?.forEach((item2) => {
+      item2.classList.remove('info-list__item--active');
+    });
+    item1.classList.add('news__item--active');
+    infoItem[index].classList.add('info-list__item--active');
+  });
+});
